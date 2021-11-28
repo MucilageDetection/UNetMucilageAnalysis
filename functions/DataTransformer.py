f20d6c500588db54bd627b8e3e256ca0072e2bd7
Bahri ABACI <abacibahri@gmail.com>
2021-11-28 08:53:11 +0000

    DataTransformer updated to normalize bands wrt 20m statistics

--- functions/DataTransformer.py
+++ functions/DataTransformer.py
@@ -2,9 +2,15 @@ from torchvision import transforms
 
 # use the following for all dataset
 def GetDataTransformer():
+
+    # sentinel stats to make range [-1,1]
+    bandMeans = [0.0381, 0.0340, 0.0237, 0.0239, 0.0219, 0.0219, 0.0207, 0.0177, 0.0159]
+    bandStds  = [0.0152, 0.0145, 0.0119, 0.0124, 0.0119, 0.0121, 0.0121, 0.0116, 0.0107]
+
+    # do transform
     trans = transforms.Compose([
         transforms.ToTensor(),
-        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # imagenet
+        transforms.Normalize(bandMeans, bandStds)
     ])
     
     return trans
